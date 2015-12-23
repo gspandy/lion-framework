5ef0aee4bbfe8b43bc01ed51ffabfc186e6b30f4
Wanglijun <lijun.wang1@newtouch.cn>
2015-12-23 10:56:15 +0800

    Redis 客户端

--- com.newtouch.lion-redis/src/test/java/com/newtouch/lion/redis/RedisClientTest.java
+++ com.newtouch.lion-redis/src/test/java/com/newtouch/lion/redis/RedisClientTest.java
@@ -29,16 +29,20 @@ public class RedisClientTest extends RedisAllTest {
     private RedisClient redisClient;
 
 
+    /**
+     * 设置了，返回OK
+     * */
     @Test
     public void set() {
-        String key = "watchmem:20150411";
-        this.redisClient.set(key, "00000000");
+        String key = "watchmem:20150410";
+        String result=this.redisClient.set(key, "00000000");
+        logger.info("result:{}",result);
     }
 
     @Test
     public void set1() {
         String key = "watchmem:201504100";
-        this.redisClient.set(key, "00000000", "", "", 10000L);
+       // this.redisClient.set(key, "00000000", "", "", 10000L);
     }
 
 
@@ -98,8 +102,17 @@ public class RedisClientTest extends RedisAllTest {
 
         // 若key不存在，则存储,存储时返回1，已存储则返回0,可以用于分布式锁
         String key="watchmem:20150411";
-        Long result=this.redisClient.setnx(key,"1234567890");
+        Long result=this.redisClient.setnx(key, "1234567890");
         logger.info("result:{}", result);
+    }
+
+    @Test
+    public void expireAt(){
+        //EXPIREAT EXPIREAT的作用和EXPIRE一样，都用于为key设置生存时间。不同在于EXPIREAT命令接受的时间参数是UNIX时间戳(unix timestamp)
+
+        String key="watchmem:20150410";
+        Long result=this.redisClient.expire(key,120);
+        logger.info("result:{}",result);
     }
 
 }
