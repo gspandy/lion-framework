bc39cf42cfdd31df38355edb352aa9f5f30120c9
scwanglijun <scwanglijun@gmail.com>
2016-08-17 14:37:28 +0800

    Redis 消息发布

---
+++ com.newtouch.lion-redis/src/main/java/com/newtouch/lion/redis/publish/Publisher.java
@@ -0,0 +1,42 @@
+package com.newtouch.lion.redis.publish;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+
+public class Publisher {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(Publisher.class);
+
+	private final Jedis publisherJedis;
+
+	private final String channel;
+
+	public Publisher(Jedis publisherJedis, String channel) {
+		this.publisherJedis = publisherJedis;
+		this.channel = channel;
+	}
+
+	public void lpublish(String msg) {
+		try {
+			publisherJedis.lpush(channel, msg);
+		} catch (Exception e) {
+			logger.error("IO failure while reading input, e");
+		}
+	}
+
+	public void publish(final String msg){
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {  
+				     Thread.sleep(1000);  
+				    } catch (InterruptedException e) {  
+				     e.printStackTrace();  
+				    } 
+				publisherJedis.publish(channel, msg);
+			}
+		}).start();
+	}
+}
---
+++ com.newtouch.lion-redis/src/main/java/com/newtouch/lion/redis/subscribe/Subscriber.java
@@ -0,0 +1,28 @@
+package com.newtouch.lion.redis.subscribe;
+
+import com.newtouch.lion.redis.listener.RedisListener;
+import redis.clients.jedis.Jedis;
+
+public class Subscriber {
+	
+	private final Jedis publisherJedis;
+
+	private final String channel;
+
+	public Subscriber(Jedis publisherJedis, String channel) {
+		this.publisherJedis = publisherJedis;
+		this.channel = channel;
+	}
+
+	public void psub(final RedisListener listener) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// 订阅得到信息在lister的onMessage(...)方法中进行处理
+				// 订阅多个频道
+				// redisClient.subscribe(listener, "news.share", "news.log");
+				publisherJedis.psubscribe(listener, new String[] {channel });// 使用模式匹配的方式设置频道
+			}
+		}).start();
+	}
+}
